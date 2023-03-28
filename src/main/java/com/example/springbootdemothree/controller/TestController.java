a43498cae226bc1e61700bc1b06519ec580f750e
wangjingyi <wangjingyi@tfzq.com>
2023-03-28 16:20:03 +0800

    全局变量

--- src/main/java/com/example/springbootdemothree/controller/TestController.java
+++ src/main/java/com/example/springbootdemothree/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.example.springbootdemothree.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TOME}")
+    private String helloTest;
+
     @GetMapping("/hello")
     //@RequestMapping(value = "/user/1",method = RequestMethod.GET)
     public String hello() {
-        return "hello word!";
+        return "hello word!" + helloTest;
     }
 
     @PostMapping("/hello/post")
