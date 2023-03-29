b171a2552f5919d4fa19e9a52fd2b2dc94ca7d52
wangjingyi <wangjingyi@tfzq.com>
2023-03-29 10:17:41 +0800

    请求参数封装EbookResp(例如客户不需要返回账户密码)

--- src/main/java/com/example/springbootdemothree/controller/EbookController.java
+++ src/main/java/com/example/springbootdemothree/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.example.springbootdemothree.controller;
 
-import com.example.springbootdemothree.domain.Ebook;
+import com.example.springbootdemothree.req.EbookReq;
 import com.example.springbootdemothree.resp.CommonResp;
+import com.example.springbootdemothree.resp.EbookResp;
 import com.example.springbootdemothree.service.EbookService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +18,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list () {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list (EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return  resp;
     }
--- src/main/java/com/example/springbootdemothree/service/EbookService.java
+++ src/main/java/com/example/springbootdemothree/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.example.springbootdemothree.service;
 
 import com.example.springbootdemothree.domain.Ebook;
+import com.example.springbootdemothree.domain.EbookExample;
 import com.example.springbootdemothree.mapper.EbookMapper;
+import com.example.springbootdemothree.req.EbookReq;
+import com.example.springbootdemothree.resp.EbookResp;
 import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +17,21 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list () {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list (EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" +req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            //ebookResp.setId(123L);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
