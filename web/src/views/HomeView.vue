63b85262d551a908b76adf2e21a73561963cf174
wangjingyi <wangjingyi@tfzq.com>
2023-03-29 15:38:02 +0800

    集成ant design vue，加入按钮样式

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <a-button type="primary" ghost>Primary</a-button>
+    <a-button ghost>Default</a-button>
+    <a-button type="dashed" ghost>Dashed</a-button>
+    <a-button danger ghost>Danger</a-button>
+    <a-button type="link" ghost>Link</a-button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
