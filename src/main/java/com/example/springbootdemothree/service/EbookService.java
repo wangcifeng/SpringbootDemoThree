94ce0abe3a5cff044abdb3b14d736e72bf0be954
wangjingyi <wangjingyi@tfzq.com>
2023-03-29 10:45:43 +0800

    制作CopyUtil封装BeanUtil

--- src/main/java/com/example/springbootdemothree/service/EbookService.java
+++ src/main/java/com/example/springbootdemothree/service/EbookService.java
@@ -5,11 +5,10 @@ import com.example.springbootdemothree.domain.EbookExample;
 import com.example.springbootdemothree.mapper.EbookMapper;
 import com.example.springbootdemothree.req.EbookReq;
 import com.example.springbootdemothree.resp.EbookResp;
+import com.example.springbootdemothree.util.CopyUtil;
 import jakarta.annotation.Resource;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,21 +16,24 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<EbookResp> list (EbookReq req) {
+    public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" +req.getName() + "%");
+        criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
+        //  List<EbookResp> respList = new ArrayList<>();
+        //  for (Ebook ebook : ebookList) {
+        //  //  EbookResp ebookResp = new EbookResp();
+        //  //  BeanUtils.copyProperties(ebook, ebookResp)
+        //  //  对象复制
+        //  EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //  respList.add(ebookResp);
+        //  }
 
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            //ebookResp.setId(123L);
-            respList.add(ebookResp);
-        }
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
 
-        return respList;
+        return list;
     }
 }
