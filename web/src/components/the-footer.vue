2ac213786293c862c3fc67d7efeae2b0d2dce3c6
wangjingyi <wangjingyi@tfzq.com>
2023-03-29 16:32:43 +0800

    4-6页面路由开发

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <a-layout-footer style="text-align: center">
+      小狗水上漂
+    </a-layout-footer>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:"the-footer",
+    // setup() {
+        
+    // },
+})
+</script>
